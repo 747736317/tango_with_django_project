d79289b41dc32d6fbb1f92b03b8eb123e3a84e37
“Yixuan <2397832d@student.gla.ac.uk>
2019-01-21 19:39:45 +0000

    Chapter 8 finished

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -7,4 +7,4 @@ register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
 def get_category_list():
-    return {'cats': Category.objects.all()}
+    return {'cats': Category.objects.all(), 'act_cat': None}
